d7cd7e948b045aae403fc46282d47c236f8e812e
rfong <rachefo@gmail.com>
2018-11-16 19:01:19 -0800

    init

---
+++ braille_img.py
@@ -0,0 +1,144 @@
+import json
+import numpy as np
+from optparse import OptionParser
+from PIL import Image
+import re
+import string
+from typedecorator import params, returns, setup_typecheck
+
+
+# https://www.pharmabraille.com/pharmaceutical-braille/the-braille-alphabet/
+
+# Maps "[0-9]" string char up in ASCII space to [a-j]
+@returns(str)
+@params(c=str)
+# `c` must be in [0-9]
+def numeral_to_character(c):
+    assert len(c)==1
+    assert 0 <= int(c) <= 9
+    return chr(ord(c) + ord('a') - ord('0'))
+
+@returns(np.ndarray)
+@params(arr=np.ndarray)
+def interleave_rows_with_zeros(arr):
+    '''Interleave a 2D numpy array with zeroed rows.'''
+    return np.hstack([arr, np.zeros_like(arr)]).reshape(
+        arr.shape[0]*2, arr.shape[1])
+
+@returns(np.ndarray)
+@params(grid=np.ndarray, spacing=int)
+def dotgrid_to_pixels(grid, spacing=1):
+    '''
+    Adds empty padding to dotgrid. `spacing` represents how many dots' width
+    of padding will go between each dot of information.
+    '''
+    # Interleave rows, transpose to interleave columns-as-rows, transpose back
+    grid = interleave_rows_with_zeros(
+        interleave_rows_with_zeros(grid).transpose()
+    ).transpose()
+    # Now chop off the last row and col, which are empty
+    return grid[0:-1, 0:-1]
+
+@returns(np.ndarray)
+@params(grid=np.ndarray, margin=int)
+def add_margin(grid, margin=1):
+    empty_row = np.zeros((margin, grid.shape[1]))
+    grid = np.vstack([empty_row, grid, empty_row])
+    empty_col = np.zeros((grid.shape[0], margin))
+    grid = np.hstack([empty_col, grid, empty_col])
+    return grid
+
+
+class BrailleImageGenerator:
+
+    # str mapping to 2x3 grid representations of braille chars.
+    #  1=dot, 0=empty
+    braille = None
+
+    def __init__(self):
+        # Load braille alphabet from json
+        if self.braille == None:
+            self.braille = json.loads(open("braille.json","r").read())
+
+    @staticmethod
+    def normalize(text):
+        '''Normalize for multi-Braille-character string characters'''
+        #chr(ord(ch) + ord('a') - ord('0'))
+
+        # Braille "[0-9]" translates up in ASCII space to "#[a-j]".
+        # The lambda extracts captures and translates group \1.
+        text = re.sub('([0-9]+)',
+            lambda match: "#" + ''.join(numeral_to_character(c) for c in match.group(1)),
+            text,
+        )
+        return text
+
+    @returns([np.ndarray])
+    @params(self=object, text=str)
+    def text_to_braille(self, text):
+        '''Translates text into braille chars represented as 2x3 numpy arrays'''
+        text = text.lower()
+        return [np.array(self.braille[ch]) for ch in text]
+
+    @returns(np.ndarray)
+    @params(self=object, brs=[np.ndarray], width=int)
+    def braille_to_dotgrid(self, brs, width=10):
+        '''
+        Wraps braille chars into a dot grid.
+        :param brs: a list of 2x3 numpy arrays each representing a braille char
+        :param width: the width, in braille chars, of the grid output
+        :returns: 2D numpy array
+        '''
+        # Pad to fully wrap to grid
+        mod = len(brs)%width
+        if mod != 0:
+            brs += [np.array(self.braille[' '])] * (width-mod)
+        # Wrap and stack together (sorry I love list comprehensions so much)
+        return np.vstack(tuple([
+            np.hstack(brs[row_index*width:(row_index+1)*width])
+            for row_index in xrange(len(brs)/width)
+        ]))
+
+    #def interleave(a, b):
+    #    '''interleave two 1D numpy arrays'''
+    #    return np.vstack((a,b)).reshape((-1,), order='F')
+
+    @returns(Image.Image)
+    @params(self=object, text=str, char_width=int, dot_margin=int)
+    def convert(self, text, char_width=10, dot_margin=1):
+        '''Convert text to a braille representation on a wrapped spaced grid'''
+        pixels = dotgrid_to_pixels(
+            self.braille_to_dotgrid(
+                self.text_to_braille(text), width=char_width))
+        pixels = add_margin(pixels, dot_margin)
+        return Image.fromarray(pixels.astype('uint8')*255)
+    #im = im.convert('L')
+    #im = ImageOps.invert(im)
+    #im = im.convert('1')
+
+
+def main():
+    parser = OptionParser(usage="usage: %prog [options] inputtext")
+    parser.add_option("-o", "--output", dest="output", type=str,
+                      help="output file path")
+    parser.add_option("--show", dest="show", action="store_true",
+                      help="show image (defaults to true if no output file specified)")
+    parser.add_option("--width", dest="width", type=int, default=10,
+                      help="grid width in braille characters")
+    parser.add_option("--margin", dest="margin", type=int, default=1,
+                      help="margin in dots")
+    (options, args) = parser.parse_args()
+    assert len(args)==1, "input text required"
+
+    setup_typecheck()
+    gen = BrailleImageGenerator()
+
+    im = gen.convert(args[0], char_width=options.width, dot_margin=options.margin)
+    if options.output:
+        im.save(options.output)
+    if options.show or not options.output:
+        im.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ braille_img_test.py
@@ -0,0 +1,66 @@
+# incomplete tests for some finicky bits in braille_img.py
+
+import unittest
+
+from braille_img import *
+
+
+class TestBrailleImageGenerator(unittest.TestCase):
+
+    def setUpModule(self):
+        setup_typecheck()
+
+    gen = None
+    def setUp(self):
+        self.gen = BrailleImageGenerator()
+
+    def test_text_to_braille(self):
+        text = "Hello! hi."  # all chars here correspond to single braille chars
+        brs = self.gen.text_to_braille(text)
+        self.assertEqual(len(brs), len(text))
+        for br in brs:
+            self.assertEqual(br.shape, (3,2))
+
+    def test_braille_to_dotgrid(self):
+        text = "Hello! hi."
+        brs = self.gen.text_to_braille(text)
+
+        # One row
+        grid = self.gen.braille_to_dotgrid(brs, width=10)
+        self.assertEqual(grid.shape, (3, 2*10))
+
+        # Two rows
+        grid = self.gen.braille_to_dotgrid(brs, width=5)
+        self.assertEqual(grid.shape, (6, 2*5))
+
+        # Two rows, with some padding
+        grid = self.gen.braille_to_dotgrid(brs, width=6)
+        self.assertEqual(grid.shape, (6, 2*6))
+
+    def test_normalize(self):
+        self.assertEqual(
+            BrailleImageGenerator.normalize("unchanged"),
+            "unchanged")
+        self.assertEqual(
+            BrailleImageGenerator.normalize("test123!"),
+            "test#bcd!")
+
+    def test_numeral_to_character(self):
+        self.assertEqual(numeral_to_character("0"), "a")
+        self.assertEqual(numeral_to_character("1"), "b")
+        self.assertEqual(numeral_to_character("2"), "c")
+        self.assertEqual(numeral_to_character("3"), "d")
+        self.assertEqual(numeral_to_character("4"), "e")
+        self.assertEqual(numeral_to_character("5"), "f")
+        self.assertEqual(numeral_to_character("6"), "g")
+        self.assertEqual(numeral_to_character("7"), "h")
+        self.assertEqual(numeral_to_character("8"), "i")
+        self.assertEqual(numeral_to_character("9"), "j")
+
+        for i in xrange(10):
+            with self.assertRaises(TypeError):
+                numeral_to_character(i)
+
+
+if __name__ == '__main__':
+    unittest.main()
